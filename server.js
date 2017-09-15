62c0b74f9341c4ce00639a61b5e6dec696803e97
ACPires <arthur98_4@hotmail.com>
2017-09-15 20:41:51 -0300

    Hiding vital informations

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://localhost/farolApi');
+require('./config/database.js')(process.env.MONGO_ACCESS);
 app.firebase = require('./config/firebase.js')();
 
 http.createServer(app).listen(app.get('port'), function(){
