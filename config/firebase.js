7499fc51e48ab311134e4d779b8ddaaf243c47fc
ACPires <arthur98_4@hotmail.com>
2017-09-15 16:48:50 -0300

    Commit to update umbler, delete after used

--- config/firebase.js
+++ config/firebase.js
@@ -4,7 +4,7 @@ var serviceAccount = require("../beacon-firebase-key.json");
 module.exports = function(){
   var firebase = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
+    databaseURL: "https://beacon-c0afe.firebaseio.com"
   });
 
   return firebase;
