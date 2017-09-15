3401557e85d96472b59e04af5ba00108103e29f1
ACPires <arthur98_4@hotmail.com>
2017-09-15 20:41:51 -0300

    Hiding vital informations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://megatron:sherlock42@mongo_farolapi:27017/farolapi');
+
+require('./config/database.js')(process.env.MONGO_ACCESS);
 app.firebase = require('./config/firebase.js')();
 
 http.createServer(app).listen(app.get('port'), function(){
