4c3ea8344ce0458db675e58030494d060ea21172
ACPires <arthur98_4@hotmail.com>
2017-10-04 12:19:34 -0300

    List bus

--- app/controllers/busStop.js
+++ app/controllers/busStop.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
 
 	const BusStop = app.models.busStop;
+	//const Bus = app.models.bus;
 	
 	var controller = {};
 	
@@ -33,9 +34,9 @@ module.exports = function(app) {
 	};
 
 	controller.listBus = function(req, res){
-		var _id = req.params.id;
+		var busline = req.params.busLine;
 		//ver melhor como vai ficar essa query
-		BusStop.findById(_id).exec()
+		BusStop.findAll({busLine: busline}).populate({path: 'busLine', select: 'busLine -_id'}).exec()
 			.then(
 				function(busstop){
 					if(!busstop) throw new Error("Nenhum veículo cadastrado ou em circulação no momento");
