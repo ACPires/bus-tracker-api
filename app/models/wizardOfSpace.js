34787aae27181bb8bca9f770be06408678ce7a48
Victor Vieira <victorvieira302@gmail.com>
2017-10-05 15:37:39 -0300

    add get correct position
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

--- app/models/wizardOfSpace.js
+++ app/models/wizardOfSpace.js
@@ -4,19 +4,72 @@ var distance = require('../util/distance');
 wizard.getNextStop = function(actualRouteStop, routePoints){
   var nextStop;
 
-  routePoints.some(function(routePoint){
+  routePoints.some(function(routePoint, index){
     if(actualRouteStop.position < routePoint.position &&
-      routePoint.busStop){
+       routePoint.busStop){
         nextStop = routePoint;
-        return true;
+        return false;
       };
   });
 
   return nextStop;
 }
 
-wizard.getCorrectPosition = function(lat, lng, routePoints){
 
+var getMiddlePoint = function(routePoints){
+  console.log(routePoints.length);
+  if(routePoints.length < 2) return -1;
+
+  return Math.round(routePoints.length / 2);
 }
+
+var getCorrectPosition = function(lat, lng, routePoints){
+  var startPoint = routePoints[0];
+  var endPoint = routePoints[routePoints.length -1];
+  var indexStart = 0;
+  var indexEnd = routePoints.length;
+
+  console.log("IndexStart: " + indexStart + " | IndexEnd: " + indexEnd);
+
+  var distanceInit = distance.distanceBetween(lat,
+                                              lng,
+                                              startPoint.latitude,
+                                              startPoint.longitude);
+
+  var distanceEnd = distance.distanceBetween(lat,
+                                             lng,
+                                             endPoint.latitude,
+                                             endPoint.longitude);
+
+  var middle = getMiddlePoint(routePoints);
+
+  console.log("middle: " + middle);
+
+  if(middle >= 0){
+
+    // var distanceMiddle = distance.distanceBetween(lat,
+    //                                               lng,
+    //                                               routePoints[middle].latitude,
+    //                                               routePoints[middle].longitude);
+
+    if(distanceInit > distanceEnd){
+      indexStart = middle;
+    }else{
+      indexEnd = middle;
+    }
+
+    return getCorrectPosition(lat, lng, routePoints.slice(indexStart, indexEnd))
+
+  }else{
+    console.log("finish search");
+    if(distanceInit > distanceEnd){
+      return startPoint;
+    }else{
+      return endPoint;
+    }
+  }
+};
+
+wizard.getCorrectPosition = getCorrectPosition;
 
 module.exports = wizard;
