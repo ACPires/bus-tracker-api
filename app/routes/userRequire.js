e58b111988e7e29678e410596691c834f3cc6b85
ACPires <arthur98_4@hotmail.com>
2017-10-07 00:45:54 -0300

    User requisition insertion

--- app/routes/userRequire.js
+++ app/routes/userRequire.js
@@ -3,10 +3,11 @@ module.exports = function(app) {
 	const controller = app.controllers.userRequire;
 	
 	app.route('/userRequire')
-		.get(controller.listRequires)
+		.get(controller.listRequires);
+	app.route('/busmodule/:busmoduleid/busstop/:busstopserial')
 		.post(controller.addRequire);
 	app.route('/userRequire/:id')
 		.get(controller.listRequires)
-		.post(controller.addRequire)
+		.post(controller.updateRequire)
 		.delete(controller.removeRequire);
 };
