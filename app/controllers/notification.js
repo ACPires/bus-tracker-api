acde80d3c2e659acee2f7f4b7d523e10e9f88d83
Victor Vieira <victorvieira302@gmail.com>
2017-10-08 17:44:26 -0300

    add service to send notification to user when bus arrived
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

--- app/controllers/notification.js
+++ app/controllers/notification.js
@@ -12,5 +12,42 @@ module.exports = function(app){
     res.status(200).json({send: 'ok'});
   };
 
+  controller.sendAlertToUser = function(req, res){
+    var id = req.params.id_user;
+
+    var request = require('request');
+    console.log(process.env.FIREBASE_CLOUD_MESSAGE_KEY);
+
+    request({
+      url: 'https://fcm.googleapis.com/fcm/send',
+      method: 'POST',
+      headers: {
+        'Content-Type' :' application/json',
+        'Authorization': process.env.FIREBASE_CLOUD_MESSAGE_KEY
+      },
+      body: JSON.stringify(
+        { "data": {
+          "body": "Seu ônibus chegou",
+          "text": "Ele está parado te aguardando"
+        },
+          "to" : id
+        }
+      )
+    }, function(error, response, body) {
+      if (error) {
+        console.error(error, response, body);
+        res.json(error)
+      }
+      else if (response.statusCode >= 400) {
+        console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage+'\n'+body);
+        res.status(response.statusCode).json(body)
+      }
+      else {
+        console.log('Done!')
+        res.json("Mensagem enviada");
+      }
+    });
+  }
+
   return controller;
 };
